c4f5fbfc52ee309a6129455bb81e9c0756f20ee9
denguir <anass.den@gmail.com>
2021-01-30 19:27:56 +0100

    mv sql query

--- mariadb-data/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE ZeusEdu;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;
-update mysql.user set plugin='mysql_native_password' where user='root';
-FLUSH PRIVILEGES;
